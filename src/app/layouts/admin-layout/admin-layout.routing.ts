d96c74081ee26f931711fa531f74a04cc12c5adf
glennfaison <glennfaison@gmail.com>
2019-10-14 14:42:45 +0100

    Implement list pages

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,33 +1,19 @@
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
-import { IconsComponent } from '../../pages/icons/icons.component';
-import { MapComponent } from '../../pages/map/map.component';
-import { NotificationsComponent } from '../../pages/notifications/notifications.component';
-import { UserComponent } from '../../pages/user/user.component';
-import { TablesComponent } from '../../pages/tables/tables.component';
-import { TypographyComponent } from '../../pages/typography/typography.component';
 import { ProfileComponent } from 'src/app/pages/profile/profile.component';
 import { StudentsComponent } from 'src/app/pages/students/students.component';
 import { InstructorsComponent } from 'src/app/pages/instructors/instructors.component';
 import { ClassroomsComponent } from 'src/app/pages/classrooms/classrooms.component';
 import { ExaminationsComponent } from 'src/app/pages/examinations/examinations.component';
 import { CoursesComponent } from 'src/app/pages/courses/courses.component';
-// import { RtlComponent } from "../../pages/rtl/rtl.component";
 
 export const AdminLayoutRoutes: Routes = [
-  { path: 'dashboard', component: DashboardComponent, },
-  { path: 'icons', component: IconsComponent, },
-  { path: 'maps', component: MapComponent, },
-  { path: 'notifications', component: NotificationsComponent, },
-  { path: 'user', component: UserComponent, },
-  { path: 'tables', component: TablesComponent, },
-  { path: 'typography', component: TypographyComponent, },
-  { path: 'my-profile', component: ProfileComponent, },
-  { path: 'students', component: StudentsComponent, },
-  { path: 'instructors', component: InstructorsComponent, },
-  { path: 'classrooms', component: ClassroomsComponent, },
-  { path: 'examinations', component: ExaminationsComponent, },
-  { path: 'courses', component: CoursesComponent, },
-  // { path: "rtl", component: RtlComponent, },
+  { path: 'dashboard', component: DashboardComponent, pathMatch: 'full', },
+  { path: 'my-profile', component: ProfileComponent, pathMatch: 'full', },
+  { path: 'students', component: StudentsComponent, pathMatch: 'full', },
+  { path: 'instructors', component: InstructorsComponent, pathMatch: 'full', },
+  { path: 'classrooms', component: ClassroomsComponent, pathMatch: 'full', },
+  { path: 'examinations', component: ExaminationsComponent, pathMatch: 'full', },
+  { path: 'courses', component: CoursesComponent, pathMatch: 'full', },
 ];
